4deb9674ee60065acea0943723481024a31dee75
Brammont <brayanmazuera1@gmail.com>
2021-11-13 11:52:57 -0500

    creando los controladores para manejar la base de datos

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,5 +6,25 @@ const findAllUsers = (req,res)=>{
         err && res.status(500).send(err.message);
         res.status(200).json(users);
     })
-}
-const findById =(req,res)
+};
+const findById = (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      err &&  res.send(500).send(err.message);
+  
+      res.status(200).json(user);
+    });
+  };
+const addUser = (req, res) => {
+    let profile = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: req.body.password,
+      
+    });
+    profile.save((err, usr) => {
+        err && res.status(500).send(err.message);
+        
+        res.status(200).json(usr);
+    });
+};
+module.exports = {findAllUsers, findById,addUser};
